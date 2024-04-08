528a2e866b0f3328fcd9e6fd7f3a489b876fe4d2
dominique Taplin <Dtaplin21@aol.com>
2024-04-08 08:43:21 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const timeLeftdisplay = querySelector('#time-left');
+const resultDisplay = querySelector('#result');
+const startPauseButton = querySelector('#start-pause-button');
+const squares = document.querySelectorAll(".grid div")
