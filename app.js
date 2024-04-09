8806c2ea7e6082ddbb2ef3d627aa2026f3588522
dominique Taplin <Dtaplin21@aol.com>
2024-04-09 07:47:33 -0700

    create movement

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ const timeLeftdisplay = querySelector('#time-left');
 const resultDisplay = querySelector('#result');
 const startPauseButton = querySelector('#start-pause-button');
 const squares = document.querySelectorAll(".grid div")
+
+function moveFrog() {
+    console.log('moved')
+    
+}
+document.addEventListener('keyup', moveFrog)
