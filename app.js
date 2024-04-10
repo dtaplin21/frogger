0d4a0b677a884f8216a9531d5208d3838211f577
dominique Taplin <Dtaplin21@aol.com>
2024-04-10 07:43:43 -0700

    add movement

--- app.js
+++ app.js
@@ -4,8 +4,30 @@ const startPauseButton = document.querySelector('#start-pause-button');
 const squares = document.querySelectorAll(".grid div")
 
 console.log(squares)
-let currentIndex = 0;
-function moveFrog() {
+let currentIndex = 76;
+let width = 9;
+
+
+function moveFrog(e) {
+console.log(e)
+    switch(e.key) {
+        case 'ArrowLeft' :
+            console.log('move Left')
+            currentIndex -= 1
+            break
+        case 'ArrowRight' :
+            console.log('move right')
+            currentIndex += 1
+            break
+        case 'ArrowUp' :
+            console.log('move up')
+            currentIndex -= width
+            break
+        case 'ArrowDown' :
+            console.log('move down');
+            currentIndex += width
+            break 
+    }
    
     squares[currentIndex].classList.add('frog')
 }
